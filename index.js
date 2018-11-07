c1a00e3a0b87bcbdb7601009c9e2daf5c9acb39a
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 15:25:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const token = process.env.token // a garder en version heroku
-const prefix = ("?");
-
-bot.on('ready', function () {
-    console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)
-    
+onst Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "?";
+ 
+client.login('TOKEN');
+ 
+client.on('message', message =>{
+    if(message.content === "tu fais quoi?"){
+        message.reply('Je mange des pâtes dans ma pastabox ! :heart:');
+        console.log('répond à tfq');
+    }
 });
-
+ 
+ 
+client.on('guildMemberAdd', member =>{
+    let embed = new Discord.RichEmbed()
+        .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
+        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
+    member.guild.channels.get('ID DU CHANNEL').send(embed)
+    member.addRole('ID DU ROLE A AJOUTER AUTOMATIQUEMENT')
+ 
+});
+ 
+client.on('guildMemberRemove', member =>{
+    let embed = new Discord.RichEmbed()
+        .setDescription(':cry: **' + member.user.username + '** a quitté ' + member.guild.name)
+        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
+    member.guild.channels.get('ID DU CHANNEL').send(embed)
+ 
+});
   
   
 
