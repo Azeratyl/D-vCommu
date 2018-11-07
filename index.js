d0f3ca9f77b8f90b7be719fa5c9b18ac414350aa
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 18:03:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,29 +8,22 @@ bot.on('message'), message => {
   
   help.parse(message)
   
-})
+});
 
 bot.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
     return channel.send('Bienvenue sur le serveur DévCommu ! Actuellement en Bêta. ' + member.displayName)
   }).catch(console.error)
   // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
-})
+});
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('!help').catch(console.error)    
-})
+});
 
  
-const ban = require('./kick et ban/ban');
 
-
-bot.on('message', function (message){
-    if (ban.match(message)){
-        return ban.action(message)
-    }
-});  
   
 
 
