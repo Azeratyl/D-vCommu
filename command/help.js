da80dcda9ff68c770f5e9531dbad914fc3152dfa
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 17:59:30 +0100

    Update help.js

--- command/help.js
+++ command/help.js
@@ -1,1 +1,25 @@
+const Discord = require('discord.js')
+const Command = require ('./command')
 
+module.exports = class Ping extends Command {
+  
+  static match(message) {
+    if(message.content === '!help') {
+      return true
+    }
+  }
+  
+  static action(message) {
+    
+    left  help = new  Discord,RichEmbded()
+          .setTitle('HELP')
+          .setDescription('Liste des commandes')
+          .addBlankField()
+          .addField('!ping, 'Renvois pong')
+          .addField('!info, 'Donne des infos')
+          .setColor('0x206694')
+          .setFooter('Fin')
+    
+    message.channel.send(help)
+  }
+}
