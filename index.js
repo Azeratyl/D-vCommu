00b9cd3d64f069df89315975f94548ee32c1667e
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 15:21:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('rien').catch(console.error)
     
-     static void UpdatePresence()
-    {
-        DiscordRichPresence discordPresence;
-        memset(&discordPresence, 0, sizeof(discordPresence));
-        discordPresence.state = "Joue en Solo";
-        Discord_UpdatePresence(&discordPresence);
-    }
 });
 
   
