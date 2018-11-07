d4d5578cc534dff7258c298c8faae1596cea05fd
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 17:13:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const bot = new Discord.Client();
 const token = process.env.token // a garder en version heroku
 const prefix = ("?");
 
+bot.on("guildMemberAdd", member => {
+  let guild = member.guild;
+  guild.defaultChannel.sendMessage(`Welcome ${member.user} to this server.`).catch(console.error);
+});
+
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('!help').catch(console.error)    
