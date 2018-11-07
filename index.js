717404a1d5b9973327ce1a121db4a39c2207d8a3
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 16:17:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('!help').catch(console.error)
 
-const ban = require('./kick et ban/ban');
-
-
-bot.on('message', function (message){
-    if (ban.match(message)){
-        return ban.action(message)
-    }
-});    
 
   
 
