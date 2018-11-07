cd0f53fed283696432592726c58e470277e4667e
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 15:27:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token // a garder en version heroku
-const prefix = ("?");
+const prefix = ("§");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)    
+    bot.user.setActivity('!help').catch(console.error)    
 });
   
   
