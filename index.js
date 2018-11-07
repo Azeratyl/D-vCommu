5c898c371e5c926ca8ac761a8fdd742d3c735b8c
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 18:52:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token // a garder en version heroku
-const prefix = ("?");
+const prefix = ("!");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)
+    bot.user.setActivity('!help Version Bêta').catch(console.error)
 });
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        return channel.send('Bienvenue sur le serveur DévCommu' + member.displayName)
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
@@ -23,7 +23,7 @@ bot.on('message', msg => {
             msg.reply('Je suis d\'accord avec toi.')
     }
     if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
+        msg.channel.send("En maintenance")
         console.log("Une personne a demandé pour aller sur ton site.")
     }
 
