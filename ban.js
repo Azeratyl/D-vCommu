f69df350da5a2e02cf9d7fde58d4db0da90b547a
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 16:02:44 +0100

    Update ban.js

--- ban.js
+++ ban.js
@@ -8,6 +8,7 @@ module.exports = class ban {
     static action (message) {
       const args = message.content.slice(prefix.length).split(/ +/);
       const command = args.shift().toLowerCase();
+      if (command --- "ban"){
         if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply('tu n\'est pas admin . ');
           const member = message.mentions.members.first();
         if (!member) return message.reply('Commande Invalide, il faut mettre `!ban @User#1234`');
@@ -16,4 +17,5 @@ module.exports = class ban {
             reason: `Banni par ${message.author.tag}`
           });
       }
+    }
 }
