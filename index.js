0f79df0176fdb5302293e235e3cfab047b2fbf76
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 17:20:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const bot = new Discord.Client();
 const token = process.env.token // a garder en version heroku
 const prefix = ("?");
 
+bot.on('guildMemberAdd', member => {
+  member.createDM().then(channel => {
+    return channel.send('Bienvenue sur mon serveur ' + member.displayName)
+  }).catch(console.error)
+  // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
+})
+
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('!help').catch(console.error)    
