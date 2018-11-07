9dffdf53365418494a0cbd6544961d5f6f146c1b
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 16:17:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const prefix = ("?");
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('!help').catch(console.error)
-    
-  
+
+const ban = require('./kick et ban/ban');
+
+
+bot.on('message', function (message){
+    if (ban.match(message)){
+        return ban.action(message)
+    }
+});    
+
   
 
 bot.login(token); //a garder en version heroku
