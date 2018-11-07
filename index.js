f94936f5bbf43f3e297ed6ce7f7383dacf795c72
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 15:21:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ bot.on('ready', function () {
         DiscordRichPresence discordPresence;
         memset(&discordPresence, 0, sizeof(discordPresence));
         discordPresence.state = "Joue en Solo";
-        discordPresence.details = "Competitive";
-        discordPresence.startTimestamp = 1507665886;
-        discordPresence.endTimestamp = 1507665886;
-        discordPresence.largeImageText = "Numbani";
-        discordPresence.smallImageText = "Rogue - Level 100";
-        discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
-        discordPresence.partySize = 1;
-        discordPresence.partyMax = 5;
         Discord_UpdatePresence(&discordPresence);
     }
 });
