e8521b744f83f381feb8a2377e9a183171bdc87a
Azeratyl <44839906+Azeratyl@users.noreply.github.com>
2018-11-07 17:26:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,41 +5,9 @@ const prefix = ("?");
 
 bot.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
-    return channel.send('Bienvenue sur mon serveur ' + member.displayName)
+    return channel.send('Bienvenue sur le serveur DévCommu ! Actuellement en Bêta. ' + member.displayName)
   }).catch(console.error)
   // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
-})
-
-bot.on('message', message => {
-
-  if (message.content.startsWith('!play')) {
-    // On récupère le premier channel audio du serveur
-    let voiceChannel = message.guild.channels
-      .filter(function (channel) { return channel.type === 'voice' })
-      .first()
-    // On récupère les arguments de la commande 
-    // il faudrait utiliser une expression régulière pour valider le lien youtube
-    let args = message.content.split(' ')
-    // On rejoint le channel audio
-    voiceChannel
-      .join()
-      .then(function (connection) {
-        // On démarre un stream à partir de la vidéo youtube
-        let stream = YoutubeStream(args[1])
-        stream.on('error', function () {
-          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-          connection.disconnect()
-        })
-        // On envoie le stream au channel audio
-        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-        connection
-          .playStream(stream)
-          .on('end', function () {
-            connection.disconnect()
-          })
-      })
-  }
-
 })
 
 bot.on('ready', function () {
